69f32c887ac13c81e848c6c88b2d8ae02859c3d9
vidhi-kumar <vidhikumar2000@gmail.com>
2022-05-22 12:53:47 +0530

    adding first file

---
+++ basic_pgm1.js
@@ -0,0 +1,13 @@
+// here we will see the execution of a simple js program
+const num = 3; // in memory a space gets the value 3 and is constant
+function multiplyBy2(inputNumber){ // multiplyBy2 is also a variable which stores the whole funtion as its value in mem.
+    const result = inputNumber*2;
+    return result;
+}
+const name = "Will";
+// all these are variables globally present in the environment. They are part of global execution context
+// for each funtion, a local execution context is generated
+
+console.log(multiplyBy2);
+
+//so far we are in global execution call stack since we didn't invoke multiplyBy2
